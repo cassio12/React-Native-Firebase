605eda7e4585064e1bb6252342b9f6f7437f4741
user <cassio12>
2021-05-24 19:25:04 -0300

    cadastro de usuario no firabase esta pronto, altenticar login

--- App.js
+++ App.js
@@ -7,12 +7,42 @@ import {decode, encode} from 'base-64'
 if (!global.btoa) {  global.btoa = encode }
 if (!global.atob) { global.atob = decode }
 
+import { firebase } from './src/firebase/config'
+
 const Stack = createStackNavigator();
 
 export default function App() {
 
   const [loading, setLoading] = useState(true)
   const [user, setUser] = useState(null)
+
+  // if (loading) {	
+  //   return (	
+  //     <></>	
+  //   )	
+  // }
+
+  // useEffect(() => {
+  //   const usersRef = firebase.firestore().collection('users');
+  //   firebase.auth().onAuthStateChanged(user => {
+  //     if (user) {
+  //       usersRef
+  //         .doc(user.uid)
+  //         .get()
+  //         .then((document) => {
+  //           const userData = document.data()
+  //           setLoading(false)
+  //           setUser(userData)
+  //         })
+  //         .catch((error) => {
+  //           setLoading(false)
+  //         });
+  //     } else {
+  //       setLoading(false)
+  //     }
+  //   });
+  // }, []);
+
 
   return (
     <NavigationContainer>
